9b0bfd18e8e14d60a5625b196977705cd1d8f725
Pawan Shrestha <pawanshresthait@gmail.com>
2021-06-18 12:12:46 +0545

    Added functionality to form for adding elements

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const form = document.forms[0];
 const list = document.querySelector('#list');
 const input = document.querySelector('#item-input');
+
+form.addEventListener('submit', e => {
+  if (input.value.length <= 0) {
+    e.preventDefault();
+    alert('Add Value First');
+    return;
+  }
+
+  e.preventDefault();
+  const newItem = document.createElement('div');
+  newItem.innerText = input.value;
+  newItem.classList.add('list-item');
+  console.log(newItem);
+  list.append(newItem);
+  input.value = '';
+});
